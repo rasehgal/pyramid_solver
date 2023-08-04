823411d69a8465a8e1312953b9853fb49792d021
rasehgal <rsehgal95@gmail.com>
2023-08-04 15:23:03 -0700

    commit initial files

---
+++ pyramid_solver.py
@@ -0,0 +1,15 @@
+def grab_pyr():
+    f = open('pyramid_sample_input.txt','r')
+    print(f.read())
+    f.close()
+
+def traverse():
+    return
+
+def confirm_multiply():
+    return
+
+def print_format():
+    return
+    
+grab_pyr()
